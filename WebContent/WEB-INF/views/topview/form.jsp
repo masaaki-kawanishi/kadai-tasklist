28026503ea695401ab44245b8a44a22dc15c451e
Kawanishi Masaaki <Mkawanishi9@gmail.com>
2018-07-22 13:13:59 +0900

    Add CreateServlet.java

--- WebContent/WEB-INF/views/topview/form.jsp
+++ WebContent/WEB-INF/views/topview/form.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
 <label for="tasks">タスク</label><br />
 <input type="text" name="tasks" value="${message.tasks}" />
 <br /><br />
@@ -9,3 +10,4 @@
 
 <input type="hidden" name="_token" value="${_token}" />
 <button type="submit">投稿</button>
+
--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,11 +2,15 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.daoutil;
+import task.message;
 
 /**
  * Servlet implementation class CreateServlet
@@ -26,11 +30,29 @@ public class CreateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String _token = (String)request.getParameter("_token");
+    	if(_token != null && _token.equals(request.getSession().getId())){
+    		EntityManager em = daoutil.createEntityManager();
+
+    		message m = new message();
+
+    		String tasks = request.getParameter("tasks");
+    		m.setTasks(tasks);
+
+    		String content = request.getParameter("content");
+    		m.setContent(content);
+
+    		em.getTransaction().begin();
+    		em.persist(m);
+    		em.getTransaction().commit();
+    		em.close();
+
+    		response.sendRedirect(request.getContextPath() + "/index");
+
+    	}
+    }
 
 
 }
